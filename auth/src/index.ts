fb70a66dc1793b75b00b4303288372802b826aef
cvssea <42877793+cvssea@users.noreply.github.com>
2020-06-26 14:13:37 +0300

    configure GC

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,10 +1,17 @@
 import express from 'express';
+import morgan from 'morgan';
 import { json } from 'body-parser';
+
+import { currentUserRouter } from './routes/currentuser';
 
 const app = express();
 const log = (...args: any[]): void => console.log(`AuthService: `, ...args);
 
+app.use(morgan('dev'));
 app.use(json());
+
+// routes
+app.use(currentUserRouter);
 
 const port = 3000;
 app.listen(port, () => {
